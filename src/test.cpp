ced33c51af0f229d5bc054320008baea14e2797b
Tryby <Tryby.Michael@epa.gov>
2022-08-19 16:24:14 -0400

    Initial commit

---
+++ src/test.cpp
@@ -0,0 +1,15 @@
+
+
+
+#define BOOST_TEST_MODULE "first_test"
+
+#include <boost/test/included/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_CASE(first_test)
+{
+  int i = 1;
+
+  BOOST_TEST(i);
+  BOOST_TEST(i == 2);
+}
